4f64b018d6cffd8bc48578cdfbe3f2f86de1ae20
tung.tt <tung.tt@altplus.com.vn>
2019-11-25 16:51:38 +0700

    header and footer and default

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>SB Admin - Dashboard</title>
-
-  <!-- Custom fonts for this template-->
-  <link href="{{url('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
-
-  <!-- Page level plugin CSS-->
-  <link href="{{url('vendor/datatables/dataTables.bootstrap4.css')}}" rel="stylesheet">
-
-  <!-- Custom styles for this template-->
-  <link href="{{url('css/sb-admin.css')}}" rel="stylesheet">
-
-</head>
+@include('common.header')
 
 <body id="page-top">
 
@@ -753,26 +731,7 @@
       </div>
     </div>
   </div>
-
   <!-- Bootstrap core JavaScript-->
-  <script src="{{url('vendor/jquery/jquery.min.js')}}"></script>
-  <script src="{{url('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-
-  <!-- Core plugin JavaScript-->
-  <script src="{{url('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
-
-  <!-- Page level plugin JavaScript-->
-  <script src="{{url('vendor/chart.js/Chart.min.js')}}"></script>
-  <script src="{{url('vendor/datatables/jquery.dataTables.js')}}"></script>
-  <script src="{{url('vendor/datatables/dataTables.bootstrap4.js')}}"></script>
-
-  <!-- Custom scripts for all pages-->
-  <script src="{{url('js/sb-admin.min.js')}}"></script>
-
-  <!-- Demo scripts for this page-->
-  <script src="{{url('js/demo/datatables-demo.js')}}"></script>
-  <script src="{{url('js/demo/chart-area-demo.js')}}"></script>
-
 </body>
 
-</html>
+@include('common.footer')
