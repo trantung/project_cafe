b068a6997c492759ac3d39c01535aaefce47f521
trantung <trantunghn196@gmail.com>
2020-10-07 19:36:11 +0700

    fix_cros

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,6 +21,10 @@ class RedirectIfAuthenticated
             return redirect('/home');
         }
 
-        return $next($request);
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', 'http://kytich.com')
+            ->header('Access-Control-Allow-Methods', '*')
+            ->header('Access-Control-Allow-Credentials', 'true')
+            ->header('Access-Control-Allow-Headers', 'X-CSRF-Token');;
     }
 }
