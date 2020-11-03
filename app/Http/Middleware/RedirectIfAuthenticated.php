72dad17548132fcc75e987970a6f67e591e46891
trantung <trantunghn196@gmail.com>
2020-11-03 23:01:59 +0700

    fix

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -22,7 +22,7 @@ class RedirectIfAuthenticated
         }
 
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'http://kytich.com')
+            ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', '*')
             ->header('Access-Control-Allow-Credentials', 'true')
             ->header('Access-Control-Allow-Headers', 'X-CSRF-Token');;
