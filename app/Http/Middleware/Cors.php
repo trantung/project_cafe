846919a0df2774f84b215d7dd2e52a09259f0469
trantung <trantunghn196@gmail.com>
2020-11-05 23:00:00 +0700

    fix

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,6 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', '*')
-            //->header('Access-Control-Allow-Credentials', 'true');
             ->header('Access-Control-Allow-Headers', '*');
     }
 }
--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,14 +17,6 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        // if (Auth::guard($guard)->check()) {
-        //     return redirect('/home');
-        // }
-//	die("demo");
         return $next($request);
-//            ->header('Access-Control-Allow-Origin', '*')
-  //          ->header('Access-Control-Allow-Methods', '*')
-    //        ->header('Access-Control-Allow-Credentials', 'true');
-           // ->header('Access-Control-Allow-Headers', 'X-CSRF-Token');;
-   }
+    }
 }
