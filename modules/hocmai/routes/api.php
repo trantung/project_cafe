e6b37017d8c0c8846c18153530eadb68afa9364f
trantung <trantunghn196@gmail.com>
2020-11-05 22:58:45 +0700

    add middelware api

--- modules/hocmai/routes/api.php
+++ modules/hocmai/routes/api.php
@@ -54,36 +54,36 @@ Route::group([
     'namespace' => 'APV\Hocmai\Http\Controllers\API',
 ], function () {
     //demo du lieu
-    Route::get('database/demo', 'HocmaiBackendController@demo');
+    Route::get('database/demo', 'HocmaiBackendController@demo')->middleware('cors');
     //Danh sách bộ lọc
-    Route::get('filter/list', 'HocmaiBackendController@getFilterList');
+    Route::get('filter/list', 'HocmaiBackendController@getFilterList')->middleware('cors');
     //Danh sách ngữ cảnh(context)
-    Route::get('context/list', 'HocmaiBackendController@getContextList');
+    Route::get('context/list', 'HocmaiBackendController@getContextList')->middleware('cors');
 
     //Create new notify
     //Step1: parameter
-    Route::post('notify/create/step1', 'HocmaiBackendController@postNotifyCreateStep1');
+    Route::post('notify/create/step1', 'HocmaiBackendController@postNotifyCreateStep1')->middleware('cors');
     //Step2
-    Route::post('notify/create/step2', 'HocmaiBackendController@postNotifyCreateStep2');
+    Route::post('notify/create/step2', 'HocmaiBackendController@postNotifyCreateStep2')->middleware('cors');
     //Step3:
-    Route::post('notify/create/step3', 'HocmaiBackendController@postNotifyCreateStep3');
+    Route::post('notify/create/step3', 'HocmaiBackendController@postNotifyCreateStep3')->middleware('cors');
     //Step4: trả về số lượng device_token sẽ được gửi
-    Route::post('notify/create/step4', 'HocmaiBackendController@postNotifyCreateStep4');
+    Route::post('notify/create/step4', 'HocmaiBackendController@postNotifyCreateStep4')->middleware('cors');
     //api lưu notify nhưng chưa gửi
-    Route::post('notify/save_not_send', 'HocmaiBackendController@postNotifySaveNotSend');
+    Route::post('notify/save_not_send', 'HocmaiBackendController@postNotifySaveNotSend')->middleware('cors');
     //Step5: api gửi notify tới firbase
-    Route::post('notify/create/step5', 'HocmaiBackendController@postNotifyCreateStep5');
+    Route::post('notify/create/step5', 'HocmaiBackendController@postNotifyCreateStep5')->middleware('cors');
 
     //test
-    Route::post('notify/test', 'HocmaiBackendController@postNotifyTest');
+    Route::post('notify/test', 'HocmaiBackendController@postNotifyTest')->middleware('cors');
 
     //import file excel cac device_token
-    Route::post('notify/import', 'HocmaiBackendController@postNotifyImport');
+    Route::post('notify/import', 'HocmaiBackendController@postNotifyImport')->middleware('cors');
     //get device_token by user hocmai_id
-    Route::post('device_token/user_hocmai_id', 'HocmaiBackendController@postDeviceTokenUserHocmaiId');
+    Route::post('device_token/user_hocmai_id', 'HocmaiBackendController@postDeviceTokenUserHocmaiId')->middleware('cors');
     //api danh sach app
     Route::get('app/list', 'HocmaiBackendController@getAppList');
     //danh sach notify
     Route::get('notify-list', 'HocmaiBackendController@getNotifyList')->middleware('cors');
-;
+    
 });
