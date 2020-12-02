6c6013a1c8088482957378ac572fb71cd2e4e1dd
trantung <trantunghn196@gmail.com>
2020-12-02 23:25:26 +0700

    fix

--- modules/hocmai/src/Services/HocmaiBackendService.php
+++ modules/hocmai/src/Services/HocmaiBackendService.php
@@ -1065,7 +1065,7 @@ class HocmaiBackendService
         if ($actionType == 15) {
             $res['school_block_id'] = $this->getInfoByActionTypeDetail($data->detail, 'school_block_id');
         }
-        if (in_array($actionType, [9,14,19])) {
+        if (in_array($actionType, [9,14, 16, 19])) {
             $res['url'] = $this->getInfoByActionTypeDetail($data->detail, 'url');
         }
         return $res;
